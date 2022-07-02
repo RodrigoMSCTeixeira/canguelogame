4ba139bed587099b13de7ef2798d99423038491b
Rodrigo Teixeira <88559255+RodrigoMSCTeixeira@users.noreply.github.com>
2022-07-02 17:31:42 -0300

    Version 1.0

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const human = document.querySelector(".human");
+const hole = document.querySelector(".hole");
+const score = document.querySelector(".score");
+let points = 0;
+
+const jump = () => {
+  human.classList.add("jump");
+
+  setTimeout(() => {
+    human.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const holePosition = hole.offsetLeft;
+  console.log(holePosition);
+  const humanPosition = +window
+    .getComputedStyle(human)
+    .bottom.replace("px", "");
+
+  if (holePosition >= -50 && holePosition < 80 && humanPosition < 40) {
+    hole.style.animation = "none";
+    hole.style.left = `${holePosition}px`;
+
+    human.style.animation = "none";
+    human.style.bottom = `${humanPosition}px`;
+
+    human.src = "../assets/images/coffin.gif";
+    human.style.width = "75px";
+    human.style.marginLeft = "50px";
+
+    clearInterval(loop);
+    lastRecord = points;
+  } else {
+    points++;
+    score.textContent = `Score: ${points}`;
+  }
+}, 10);
+
+document.addEventListener("keydown", jump);
